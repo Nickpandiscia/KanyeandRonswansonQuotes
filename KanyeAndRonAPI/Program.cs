6694c7120d4bde57ca4e55c9dc526f1ba9819717
Nickpandiscia <npandiscia1@gmail.com>
2022-11-09 11:32:28 -0500

    initial commit

---
+++ KanyeAndRonAPI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeAndRonAPI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Welcome to the famous person quote generator.");
+            Console.WriteLine("Today, we have Ron Swanson and Kanye West quotes.");
+            Console.WriteLine("Which persons quotes do you wanna look at?");
+
+            var userResponse = Console.ReadLine();
+
+            if(userResponse == "kanye".ToLower())
+            {
+                QuotesFromCelebs.KanyeQuotes();
+            }
+
+            if(userResponse == "ron swanson".ToLower())
+            {
+                QuotesFromCelebs.RonSwansonQuotes();
+            }
+        }
+    }
+}
---
+++ KanyeAndRonAPI/QuotesFromCelebs.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanyeAndRonAPI
+{
+    public  class QuotesFromCelebs
+    {
+        public static void KanyeQuotes()
+        {
+            var client = new HttpClient(); //uses System.Net.Http; Helps you make a request to the internet.
+
+            var kanyeURL = "https://api.kanye.rest"; //Where we will get our JSON info.
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result; //we send a GET request to the specified Uri and return the response body as a string in an asynchronous operation.  Basically, we get a string of JSON back
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"Kanye: {kanyeQuote}");
+        }
+
+        public static void RonSwansonQuotes()
+        {
+            var client = new HttpClient();
+
+            var RonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(RonURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($" Ron: {ronQuote}");
+        }
+
+    }
+}
